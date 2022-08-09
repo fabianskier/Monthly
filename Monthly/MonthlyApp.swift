1acfb459fabf0957cb2d1719d8b665c5bc3a0d5e
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-08 22:13:25 -0400

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Oscar Cristaldo on 2022-08-08.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
